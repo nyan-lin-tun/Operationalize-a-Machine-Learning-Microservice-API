0630aec76e60c6092273aa827dfbff0dbaca2404
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-05-16 23:17:03 +0630

    Finished upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=nyanlintun/mlmicroservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username nyanlintun
+docker tag mlmicroservice $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
